08f99aa999e24220c979553f76d9dfbfba54aa0f
casselsg <casselsg19@bcdschool.org>
2018-07-17 15:47:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+  get '/' do 
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is butt"
+  end
+    
+  get '/hometown' do
+    "My hometown is Manhattan"
+  end
+  
+  get '/favorite song' do 
+    "My favorite song is Heart Attack"
+  end
+  
+end 
